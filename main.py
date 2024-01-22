cdd192e76f731b652183a239a816e5275b6e550f
SW33TSTUFF <simaakniyaz@gmail.com>
2024-01-22 20:52:53 +0530

    Add UI changes and update computer opponent's logic

--- main.py
+++ main.py
@@ -1,165 +1,152 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import ImageTk, Image
 import random
+import sys
+import os
 
 root = Tk()
 root.title('TIC TAC TOE')
-# root.iconbitmap('path_to_icon.ico')  
 root.resizable(False, False)
 
-# X starts so true
+path = os.path.join(sys.path[0], "Assets/zim.png")
+icon = Image.open(path)
+icon = icon.resize((32, 32))  
+icon = ImageTk.PhotoImage(icon)
+root.iconphoto(True, icon)
+
+
+def center_window(window):
+    window_width = 500  
+    window_height = 400  
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x_coordinate = int((screen_width / 2) - (window_width / 2))
+    y_coordinate = int((screen_height / 2) - (window_height / 2))
+    window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+
 clicked = True
 count = 0
 
-# disable all buttons
 def disableButtons():
     for button in buttons:
         button.config(state=DISABLED)
 
-
 winning_combinations = [
-    [0, 1, 2],  # 1st row
-    [3, 4, 5],  # 2nd row
-    [6, 7, 8],  # 3rd row
-    [0, 3, 6],  # 1st column
-    [1, 4, 7],  # 2nd column
-    [2, 5, 8],  # 3rd column
-    [0, 4, 8],  # top to bottom diagonal
-    [6, 4, 2]   # bottom to top diagonal
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]
 ]
 
-# Check for win status
 def checkWin():
     global winner
     winner = False
-
     for combination in winning_combinations:
-        if all(buttons[i]["text"] == "X" for i in combination): # Checks for all the combinations directly
+        if all(buttons[i]["text"] == "X" for i in combination):
             for i in combination:
-                buttons[i].config(bg="green")
+                buttons[i].config(bg="#80FF80")
             winner = True
             messagebox.showinfo("Tic Tac Toe", "Player win!")
             disableButtons()
             break
         elif all(buttons[i]["text"] == "O" for i in combination):
             for i in combination:
-                buttons[i].config(bg="green")
+                buttons[i].config(bg="#80FF80")
             winner = True
             messagebox.showinfo("Tic Tac Toe", "Computer win!")
             disableButtons()
             break
-
-    # Check if tie
     if count == 9 and not winner:
         messagebox.showinfo("Tic Tac Toe", "It's a Tie!")
         disableButtons()
 
-
 def checkPlayerWin():
-    # global winner
-    # winner = False
-
     for combination in winning_combinations:
-        if all(buttons[i]["text"] == "X" for i in combination): # Checks for all the combinations directly
+        if all(buttons[i]["text"] == "X" for i in combination):
             return True
-        
-    # Check if tie (Probably repetitive)
     if count == 9 and not winner:
         return False
-    
-def checkComputerWin():
-    # global winner
-    # winner = False
 
+def checkComputerWin():
     for combination in winning_combinations:
         if all(buttons[i]["text"] == "O" for i in combination): 
             return True
-        
-    # Check if tie
     if count == 9 and not winner:
         return False
-
-def checkTie():
-    if count == 9 and not winner:
-        messagebox.showinfo("Tic Tac Toe", "It's a Tie!")
-        disableButtons()
 
 def compMove():
     global count
     available_spots = [i for i in range(len(buttons)) if buttons[i]["text"] == " "]
-    
     if available_spots:
-        #print(available_spots)
-        #random_index = random.choice(available_spots)
-        # Check for spots where the computer can win immediately before the player
         entryFlag = True
         if(entryFlag == True):
             for i in available_spots:
                 buttons[i]["text"] = "O"
+                buttons[i].config(fg="#ed5555")
                 if (checkComputerWin()):
                     entryFlag = False
                     count += 1
                     break
                 else:
-                    buttons[i]["text"] = " " # Leave it as it was before
-        
-        # if there is no such winning condition for the computer then look for winning conditions for the player
+                    buttons[i]["text"] = " "
+                    buttons[i].config(fg="#ffca4f")
         if(entryFlag == True):
             for i in available_spots:
                 buttons[i]["text"] = "X"
                 if (checkPlayerWin()):
                     entryFlag = False
-                    buttons[i]["text"] = "O" # Dont let the player get that win
+                    buttons[i]["text"] = "O"
+                    buttons[i].config(fg="#ed5555")
                     count += 1
                     break
                 else:
-                    buttons[i]["text"] = " " # Leave it as it was before
-
-        # If both conditions dont exist, chose a random positon available
+                    buttons[i]["text"] = " "
+                    buttons[i].config(fg="#ffca4f")
         if(entryFlag == True):
             entryFlag = False
             random_index = random.choice(available_spots)
             buttons[random_index]["text"] = "O"
+            buttons[random_index].config(fg="#ed5555")
             count += 1
-                
 
-# Button clicked function
 def b_click(b):
     global count
-    #checkWin()
     if (b["text"] == " ") and (count%2==0):
         b["text"] = "X"
         count += 1
-        #checkWin()
-        compMove()
         checkWin()
+        if not checkPlayerWin():
+            compMove()
+        if (checkComputerWin() and not checkPlayerWin()):
+            checkWin()
     else:
         messagebox.showerror("Tic Tac Toe", "That box has already been selected\nPick another box...")
 
-
-# Reset the game
 def reset():
     global clicked, count
     clicked = True
     count = 0
     for button in buttons:
-        button.config(text=" ", bg="silver", state=NORMAL)
+        button.config(text=" ", bg="#343638", state=NORMAL)
 
-# Build our buttons
-buttons = [Button(root, text=" ", font=("Helvetica", 20), height=4, width=8, bg="silver", command=lambda x=i: b_click(buttons[x])) for i in range(9)]
+buttons = [Button(root, text=" ", font=("Nevis", 30, "bold"), width=8, fg="#ffca4f", cursor="hand2", activebackground="#81a0a6", command=lambda x=i: b_click(buttons[x])) for i in range(9)]
 
-# Grid our buttons to the screen
+root.columnconfigure(0, weight = 1)
+root.columnconfigure(1, weight = 1)
+root.columnconfigure(2, weight = 1)
+root.rowconfigure(0, weight = 1)
+root.rowconfigure(1, weight = 1)
+root.rowconfigure(2, weight = 1)
+
 for i in range(3):
     for j in range(3):
-        buttons[i*3 + j].grid(row=i, column=j)
+        buttons[i*3 + j].config(bg='#343638')
+        buttons[i*3 + j].grid(row=i, column=j, sticky='nsew')
 
-# Create a menu
-myMenu = Menu(root)
+myMenu = Menu(root, font=('Arial', 13))
 root.config(menu=myMenu)
 
-# Create options menu
-optionMenu = Menu(myMenu, tearoff=False)
+optionMenu = Menu(myMenu, tearoff=False, font=('Arial', 13))
 myMenu.add_cascade(label="Options", menu=optionMenu)
 optionMenu.add_command(label="Reset Game", command=reset)
 
+center_window(root)
 root.mainloop()
